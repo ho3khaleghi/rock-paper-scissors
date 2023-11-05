53ab071aa4feb73edb4e145aeb40c38509599055
ho3khaleghi <>
2023-11-05 11:03:02 +0000

    Made the functions that needed in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log("Hello world!");
+// Game Run
+
+// Game Logic
+// Random computer choice
+function ComputerTurn() {
+
+}
+
+// Compare the user choice and the computer choice
+function checkWinner() {
+
+}
+
+// Result part
+function correctScore() {
+
+}
+
+function incorrectScore() {
+
+}
+
+function roundCounter() {
+    
+}
