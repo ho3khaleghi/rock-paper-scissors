2f86b5cf155e676acfbbbaca2ba2310ed99db30f
ho3khaleghi <>
2023-11-10 17:59:40 +0000

    Added the elements that was created by js to the HTML, fixed some errors in score counters

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,22 @@ let playerText = document.getElementById("player-text");
 let computerText = document.getElementById("computer-text");
 let resultText = document.getElementById("result-text");
 let btns = document.querySelectorAll(".choice-btn");
-let userScore = document.getElementById("user-score");
-let computerScore = document.getElementById("computer-score");
+let userScoreElement = document.getElementById("user-score");
+let computerScoreElement = document.getElementById("computer-score");
+let userScore = 0;
+let computerScore = 0;
 let roundScore = document.getElementById("round-score");
 let container = document.getElementById("container");
 let starting = document.getElementById("starting");
+let matching = document.getElementById("matching");
+let end = document.getElementById("end");
+let bestOfOne = document.getElementById("bo1");
+let bestOfThree = document.getElementById("bo3");
+let bestOfFive = document.getElementById("bo5");
+let startButton = document.getElementById("start-button");
 let player;
 let computer;
-let matchEnd;
+let pageStatus = "starting";
 let userNameSpan = document.getElementById("username-input");
 
 // Making a variable for the icons to have less complicated code
@@ -29,6 +37,9 @@ function handleClickButton(button) {
     computerText.innerHTML = computer;
     resultText.innerHTML = checkWinner();
     roundLimit();
+    if (pageStatus == "end") {
+        checkStatus();
+    }
 }
 
 function addClickListener(button) {
@@ -97,13 +108,13 @@ function checkWinner() {
 
 // Result part
 function correctScore() {
-    let oldScore = parseInt(userScore.innerHTML);
-    userScore.innerHTML = ++oldScore;
+    userScore++;
+    userScoreElement.innerHTML = userScore;
 }
 
 function incorrectScore() {
-    let oldScore = parseInt(computerScore.innerHTML);
-    computerScore.innerHTML = ++oldScore;
+    computerScore++;
+    computerScoreElement.innerHTML = computerScore;
 }
 
 function roundCounter() {
@@ -112,95 +123,123 @@ function roundCounter() {
 }
 
 // Building the landing page
-startingView = document.createElement("div");
-startingView.id = "starting-view";
-starting.appendChild(startingView);
+// startingView = document.createElement("div");
+// startingView.id = "starting-view";
+// starting.appendChild(startingView);
 
-// Input of the landing page
-let userName = document.createElement("input");
-userName.type = "text";
-userName.name = "user-name";
-userName.id = "username";
-userName.placeholder = "Enter your name...";
-userName.required = true;
-userName.maxLength = "10";
-let userNameValue = userName.value;
-userNameSpan.innerHTML = userNameValue;
-startingView.appendChild(userName);
+// // Input of the landing page
+// let userName = document.createElement("input");
+// userName.type = "text";
+// userName.name = "user-name";
+// userName.id = "username";
+// userName.placeholder = "Enter your name...";
+// userName.required = true;
+// userName.maxLength = "10";
+// let userNameValue = userName.value;
+// userNameSpan.innerHTML = userNameValue;
+// startingView.appendChild(userName);
 
-// Match option buttons in landing page
-let optionButtonContainer = document.createElement("div");
-optionButtonContainer.className = "btn-container";
-starting.appendChild(optionButtonContainer);
-let bestOfOne = document.createElement("button");
-let bestOfThree = document.createElement("button");
-let bestOfFive = document.createElement("button");
-bestOfOne.innerHTML = `<i class="fa-solid fa-dice-one" id="ibo1" value="1"></i>`;
-let iBo1Btn = document.getElementById("ibo1");
-bestOfOne.id = "bo1";
-bestOfOne.value = "1";
-bestOfThree.innerHTML = `<i class="fa-solid fa-dice-three"></i>`;
-bestOfThree.id = "bo3";
-bestOfThree.value = "3"
-bestOfFive.innerHTML = `<i class="fa-solid fa-dice-five"></i>`;
-bestOfFive.id = "bo5";
-bestOfFive.value = "5";
-optionButtonContainer.appendChild(bestOfOne);
-optionButtonContainer.appendChild(bestOfThree);
-optionButtonContainer.appendChild(bestOfFive);
+// // Match option buttons in landing page
+// let optionButtonContainer = document.createElement("div");
+// optionButtonContainer.className = "btn-container";
+// starting.appendChild(optionButtonContainer);
+// let bestOfOne = document.createElement("button");
+// let bestOfThree = document.createElement("button");
+// let bestOfFive = document.createElement("button");
+// bestOfOne.innerHTML = `<i class="fa-solid fa-dice-one" id="ibo1" value="1"></i>`;
+// let iBo1Btn = document.getElementById("ibo1");
+// bestOfOne.id = "bo1";
+// bestOfOne.value = "1";
+// bestOfThree.innerHTML = `<i class="fa-solid fa-dice-three"></i>`;
+// bestOfThree.id = "bo3";
+// bestOfThree.value = "3";
+// bestOfFive.innerHTML = `<i class="fa-solid fa-dice-five"></i>`;
+// bestOfFive.id = "bo5";
+// bestOfFive.value = "5";
+// optionButtonContainer.appendChild(bestOfOne);
+// optionButtonContainer.appendChild(bestOfThree);
+// optionButtonContainer.appendChild(bestOfFive);
 
-// Rules part in landing page
-let rulesContainer = document.createElement("div");
-rulesContainer.className = "rules-container";
-starting.appendChild(rulesContainer);
-let rulesTitle = document.createElement("h4");
-rulesTitle.textContent = "Rules:";
-rulesContainer.appendChild(rulesTitle);
-let rulesList = document.createElement("ul");
-rulesContainer.appendChild(rulesList);
-let rulesItem1 = document.createElement("li");
-rulesItem1.textContent = "You have 3 choices (Rock, Paper, Scissors)";
-rulesList.appendChild(rulesItem1);
-let rulesItem2 = document.createElement("li");
-rulesItem2.textContent = "You have 3 option (Bo1, Bo3, Bo5). Bo1 is only 1 round match. Bo3 is 3 rounds match if your score reach 2 you are the winner. Bo5 is similar to Bo3 but if your score reach 3 you will be winner.";
-rulesList.appendChild(rulesItem2);
-let rulesItem3 = document.createElement("li");
-rulesItem3.textContent = "Just remember 1. Rock beats Scissors. 2. Paper beats Rock. 3. Scissors beats Paper.";
-rulesList.appendChild(rulesItem3);
+// // Rules part in landing page
+// let rulesContainer = document.createElement("div");
+// rulesContainer.className = "rules-container";
+// starting.appendChild(rulesContainer);
+// let rulesTitle = document.createElement("h4");
+// rulesTitle.textContent = "Rules:";
+// rulesContainer.appendChild(rulesTitle);
+// let rulesList = document.createElement("ul");
+// rulesContainer.appendChild(rulesList);
+// let rulesItem1 = document.createElement("li");
+// rulesItem1.textContent = "You have 3 choices (Rock, Paper, Scissors)";
+// rulesList.appendChild(rulesItem1);
+// let rulesItem2 = document.createElement("li");
+// rulesItem2.textContent = "You have 3 option (Bo1, Bo3, Bo5). Bo1 is only 1 round match. Bo3 is 3 rounds match if your score reach 2 you are the winner. Bo5 is similar to Bo3 but if your score reach 3 you will be winner.";
+// rulesList.appendChild(rulesItem2);
+// let rulesItem3 = document.createElement("li");
+// rulesItem3.textContent = "Just remember 1. Rock beats Scissors. 2. Paper beats Rock. 3. Scissors beats Paper.";
+// rulesList.appendChild(rulesItem3);
 
-// Start Button in landing page
-let startButton = document.createElement("button");
-startButton.className = "glowing-btn";
-startButton.innerHTML = `<span class='glowing-txt'>S<span class='faulty-letter'>T</span>ART</span>`;
-starting.appendChild(startButton);
+// // Start Button in landing page
+// let startButton = document.createElement("button");
+// startButton.className = "glowing-btn";
+// startButton.innerHTML = `<span class='glowing-txt'>S<span class='faulty-letter'>T</span>ART</span>`;
+// starting.appendChild(startButton);
 
 // buttons handleclick to store their value
 let matchOption;
 function handleMatchOption(e) {
     matchOption = e.target.value;
-    console.log(matchOption);
 }
 
 bestOfOne.addEventListener("click", handleMatchOption);
 bestOfThree.addEventListener("click", handleMatchOption);
 bestOfFive.addEventListener("click", handleMatchOption);
-iBo1Btn.addEventListener("click", handleMatchOption);
+
 
 
 // Setting round limit
 function roundLimit() {
     if (matchOption == "1") {
-        if (userScore == "1" || computerScore == "1") {
-            matchEnd = "end";
+        if (userScore === 1 || computerScore === 1) {
+            pageStatus = "end";
         }
     } else if (matchOption == "3") {
-        if (userScore == "2" || computerScore == "2") {
-            matchEnd = "end";
+        if (userScore === 2 || computerScore === 2) {
+            pageStatus = "end";
         }
     } else if (matchOption == "5") {
-        if (userScore == "3" || computerScore == "3") {
-            matchEnd = "end";
+        if (userScore === 3 || computerScore === 3) {
+            pageStatus = "end";
         }
     }
 }
 
+// Start button
+function handleStartClick() {
+    pageStatus = "matching";
+    checkStatus();
+}
+
+startButton.addEventListener("click", handleStartClick);
+
+function checkStatus() {
+    switch (pageStatus) {
+        case "starting":
+            matching.style.display = "none";
+            end.style.display = "none";
+            starting.style.display = "block";
+            break;
+        case "matching":
+            starting.style.display = "none";
+            end.style.display = "none";
+            matching.style.display = "block";
+            break;
+        case "end":
+            starting.style.display = "none";
+            matching.style.display = "none";
+            end.style.display = "block";
+            break;
+    }
+}
+
+checkStatus();
