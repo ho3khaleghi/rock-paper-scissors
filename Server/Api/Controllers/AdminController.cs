d1bbbe07dd20cd564baef57d7358148a93acce3a
Ali Khaleghifard <alikhaleghifard@gmail.com>
2024-12-06 17:01:58 +0200

    Server side project has been added. User CRUD functionality ihas been implemented.

---
+++ Server/Api/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RockPaperScissorsApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "Get")]
+        public string Get()
+        {
+            _logger.LogInformation("You were here!");
+            _logger.LogWarning("Someone was here!");
+            return "You are here!";
+        }
+
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+    }
+}
