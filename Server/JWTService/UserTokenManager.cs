ac293505b3f2c95c5f967b39dc8e4bdc0dc1a4b3
AzureAD\AliKhaleghifard <ali.khaleghifard@prog-it.net>
2025-02-17 18:11:39 +0200

    UserTokenManager has been implemented.

---
+++ Server/JWTService/UserTokenManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JWTService.Model;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTService
+{
+    public class UserTokenManager
+    {
+        private readonly TokenKeyManager _tokenKeyManager;
+        private readonly ILogger<UserTokenManager> _logger;
+
+        public UserTokenManager(TokenKeyManager tokenKeyManager, ILogger<UserTokenManager> logger)
+        {
+            _tokenKeyManager = tokenKeyManager;
+            _logger = logger;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_tokenKeyManager.CurrentActiveKey.SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                [
+                    new Claim(ClaimTypes.Name, user.Username),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+                ]),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        public ClaimsPrincipal? GetPrincipal(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_tokenKeyManager.CurrentActiveKey.SecretKey);
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            try
+            {
+                var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+                var jwtSecurityToken = securityToken as JwtSecurityToken;
+
+                if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    throw new SecurityTokenException("Invalid token");
+                }
+
+                return principal;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error validating token");
+                return null;
+            }
+        }
+    }
+}
---
+++ Server/JWTService/UserTokenRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JWTService.Model;
+
+namespace JWTService
+{
+    public class UserTokenRepository
+    {
+        private readonly ConcurrentDictionary<User, ClaimsIdentity> _tokens
+            = new ConcurrentDictionary<User, ClaimsIdentity>();
+        public UserTokenRepository() { }
+    }
+}
